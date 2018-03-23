1f872bdfdb852a46f5487717aeccca902399eb60
Flujo <flujo@Flujos-MacBook-Air.local>
2018-03-23 18:17:14 +0530

    commit from himakar

--- src/app/analytics/analytics.component.ts
+++ src/app/analytics/analytics.component.ts
@@ -7,6 +7,7 @@ import {Observable} from 'rxjs/Observable';
 import * as _ from 'underscore';
 import * as moment from 'moment';
 
+
 @Component({
   selector: 'app-analytics',
   templateUrl: './analytics.component.html',
@@ -16,31 +17,14 @@ export class AnalyticsComponent implements OnInit {
   timeRange = 'option2';
   isActive = true;
 
-
+  //post params for analytics ajax data
   params = {
     client_id: AppConstants.CLIENT_ID,
     from_date: '2000-01-01',
-    to_date: '2018-03-31',
-    problem_type: ['water', 'power', 'road']
+    to_date: '2018-03-31'
   }
   
-  data: any = [
-    {id: 1, solved: 10, pending:20, problems:50,category:"water"},
-    {id: 2, solved: 10, pending:20, problems:50,category:"power"},
-    {id: 3, solved: 10, pending:20, problems:50,category:"sanitation"},
-    {id: 4, solved: 10, pending:20, problems:50,category:"Drinage"},
-    {id: 5, solved: 10, pending:20, problems:50,category:"roads"},
-    {id: 6, solved: 10, pending:20, problems:50,category:"manholes"},
-    {id: 7, solved: 10, pending:20, problems:50,category:"test"},
-    {id: 8, solved: 10, pending:20, problems:50,category:"test"},
-    {id: 8, solved: 10, pending:20, problems:50,category:"test"},
-    {id: 8, solved: 10, pending:20, problems:50,category:"test"},
-    {id: 8, solved: 10, pending:20, problems:50,category:"test"},
-    {id: 8, solved: 10, pending:20, problems:50,category:"test"},
-    {id: 8, solved: 10, pending:20, problems:50,category:"test"},
-    {id: 8, solved: 10, pending:20, problems:50,category:"test"},
-  ];
-
+  AgeData: any = [];
   newData : any = [];
   problem_category : any;
   constructor(public http: HttpClient) { }
@@ -53,6 +37,7 @@ export class AnalyticsComponent implements OnInit {
     this.isActive = !this.isActive;
   }
 
+
   getData(params) {
     this.http.post<Observable<any[]>>('http://www.flujo.in/dashboard/flujo.in_api_client/flujo_client_postreportanalytics', params)
     .subscribe(
@@ -61,7 +46,8 @@ export class AnalyticsComponent implements OnInit {
         this.newData = response;
         this.problem_category = this.newData[0].problem_category;
         console.log(JSON.stringify(this.problem_category));
-        
+        this.AgeData = this.newData[2].age;
+        console.log(JSON.stringify(this.AgeData));
       },
       error => {
         console.log(error);
