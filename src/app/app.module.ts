136d2aad4a8c2c8c7622c7a581ee61d5ecf6c83f
Apple <yogesh@flujo.in>
2018-03-30 11:10:43 +0530

    solved MatTableDataSource error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,8 +25,11 @@ import { EmailserviceComponent } from './emailservice/emailservice.component';
 import { Ng4LoadingSpinnerModule } from 'ng4-loading-spinner';
 import { NgxSmartLoaderModule, NgxSmartLoaderService } from 'ngx-smart-loader';
 import { MatButtonModule, MatFormFieldModule, MatInputModule,
-  // tslint:disable-next-line:max-line-length
-  MatDialogModule, MatSlideToggleModule, MatProgressBarModule, MatDatepickerModule, MatPaginatorModule, MatTableModule, MatSortModule, MatNativeDateModule, MatExpansionModule, DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE,  MatSelectModule, MatMenuModule, MatTabsModule, MatCardModule, MatTooltipModule, MatAutocompleteModule } from '@angular/material';
+    MatDialogModule, MatSlideToggleModule, MatProgressBarModule,
+    MatDatepickerModule, MatPaginatorModule, MatTableModule, MatSortModule, 
+    MatNativeDateModule, MatExpansionModule, DateAdapter,
+    MAT_DATE_FORMATS, MAT_DATE_LOCALE,  MatSelectModule, MatMenuModule, MatTabsModule, 
+    MatCardModule, MatTooltipModule, MatAutocompleteModule } from '@angular/material';
 import { NgIdleKeepaliveModule } from '@ng-idle/keepalive';
 // this includes the core NgIdleModule but includes keepalive providers for easy wireup
 import { MomentModule } from 'angular2-moment'; // optional, provides moment-style pipes for date formatting
