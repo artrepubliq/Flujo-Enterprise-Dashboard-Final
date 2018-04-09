3261ae85c5ba1aa19a4da511e2c368cfa26e5caa
Krishna Kanth <yogesh@flujo.in>
2018-04-09 11:01:38 +0530

    updated sidebar - default close and hover effect

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -180,6 +180,13 @@ export class AdminComponent implements OnInit {
     return  this.httpClient.get<Array<IAccessLevelModel>>(AppConstants.API_URL + '/flujo_client_getuseraccess/' + AppConstants.CLIENT_ID);
   }
 
+  sidebarToggleOpen(){
+    this.sidebarToggledButton = true;
+    console.log('mouse enter to sidebar')
+  }
+  sidebarToggleClose() {
+    this.sidebarToggledButton = false;
+  }
 }
 @Component({
   // tslint:disable-next-line:component-selector
