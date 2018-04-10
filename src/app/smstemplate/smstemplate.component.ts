459526e215361f27fb8008bb1ad4eb900220bef6
flujo <flujo@flujos-MacBook-Air-3.local>
2018-04-10 20:26:10 +0530

    added existing files

--- src/app/smstemplate/smstemplate.component.ts
+++ src/app/smstemplate/smstemplate.component.ts
@@ -72,30 +72,7 @@ export class SmstemplateComponent implements OnInit {
     });
   }
 
-  editTemplate(text) {
 
-
-
-  heroine: TemplateData = {
-    id: 1,
-    header: 'Tom',
-    message: 'On behalf of Up coming Good Friday, We truly wishes a Happy n Joyful day',
-    footer: '***From Flujo***'
-  };
-
-  templatestring: string;
-  public templateText: string;
-  header: string;
-  message: string;
-  footer: string;
-
-  constructor() {
-    this.header = 'Good Friday';
-    this.message = 'On behalf of Up coming Good Friday, We truly wishes a Happy n Joyful day';
-    this.footer = '***From Flujo***';
-    this.templateText = '';
-    this.templatestring = this.header + this.message + this.footer;
-  }
 
   editTemplate( text ) {
     console.log((this.templatestring));
