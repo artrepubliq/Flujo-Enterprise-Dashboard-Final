ebfad134a3d48e0e16328df786b1217449c25ecf
Yogesh <yogesh@flujo.in>
2018-04-10 14:12:41 +0530

    email template config

--- src/app/email-template/email-template-service.ts
+++ src/app/email-template/email-template-service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { AppConstants } from '../app.constants';
 import { Observable } from 'rxjs/Observable';
-import { EmailThemeConfig } from '../model/emailThemeConfig.model';
+import { IPostEmailTemplate } from '../model/emailThemeConfig.model';
 import { IHttpResponse } from '../model/httpresponse.model';
 
 @Injectable()
@@ -10,14 +10,14 @@ export class EmailTemplateService {
 
     constructor(private httpClient: HttpClient) { }
 
-    public getTemplateConfigData(request_url: string, client_id: string): Observable<EmailThemeConfig> {
-        return this.httpClient.get<EmailThemeConfig>(AppConstants.API_URL + request_url + client_id);
+    public getTemplateConfigData(request_url: string, client_id: string): Observable<IPostEmailTemplate> {
+        return this.httpClient.get<IPostEmailTemplate>(AppConstants.API_URL + request_url + client_id);
     }
 
     public postEmailTemplateData( post_data: string, request_url: string) {
-        return this.httpClient.post<IHttpResponse>(AppConstants.API_URL + request_url, post_data);
+        return this.httpClient.post<IPostEmailTemplate>(AppConstants.API_URL + request_url, post_data);
     }
     public deleteEmailTemplateData( post_data: string, request_url: string, emailtemplateconfig_id: string) {
-        return this.httpClient.delete<IHttpResponse>(AppConstants.API_URL + request_url + emailtemplateconfig_id);
+        return this.httpClient.delete<IPostEmailTemplate>(AppConstants.API_URL + request_url + emailtemplateconfig_id);
     }
 }
