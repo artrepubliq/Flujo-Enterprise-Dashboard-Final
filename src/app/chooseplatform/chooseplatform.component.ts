c017d9b5c0b5958ce82ace923fd7bae6483bbc65
Ajay Reddy <ajay@fujo.in>
2018-04-10 19:09:48 +0530

    Editor choosing

--- src/app/chooseplatform/chooseplatform.component.ts
+++ src/app/chooseplatform/chooseplatform.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
 import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
 import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
-
+import { EditorSelectionService } from '../service/editor-selection.service';
 @Component({
   selector: 'app-chooseplatform',
   templateUrl: './chooseplatform.component.html',
@@ -9,15 +9,22 @@ import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
 })
 export class ChooseplatformComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private spinnerService: Ng4LoadingSpinnerService,
+    public editorSelect: EditorSelectionService
+  ) { }
 
   ngOnInit() {
-    setTimeout(function() {
+    setTimeout(function () {
       this.spinnerService.hide();
     }.bind(this), 3000);
   }
 
-  routing () {
+  routing(editorType) {
+    this.editorSelect.onClick(editorType, true);
+    console.log(editorType);
+    localStorage.setItem('editor_source', editorType);
     this.router.navigate(['admin/pages']);
   }
 }
