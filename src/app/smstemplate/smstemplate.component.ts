7dafc5e2d6ada9ed5f5bbbc2193a1446fb5e8c9e
Yogesh <yogesh@flujo.in>
2018-04-12 10:54:36 +0530

    Bug fixes

--- src/app/smstemplate/smstemplate.component.ts
+++ src/app/smstemplate/smstemplate.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
 import { Observable } from 'rxjs/Observable';
 import { startWith } from 'rxjs/operators/startWith';
@@ -12,21 +11,12 @@ import { IHttpResponse } from '../model/httpresponse.model';
 import { AlertService } from 'ngx-alerts';
 import { ISmsTemplateData } from '../model/smsTemplateData';
 import * as _ from 'underscore';
-
-export class TemplateData {
-  id: number;
-  header: string;
-  message: string;
-  footer: string;
-}
-
 @Component({
   selector: 'app-smstemplate',
   templateUrl: './smstemplate.component.html',
   styleUrls: ['./smstemplate.component.scss']
 })
 export class SmstemplateComponent implements OnInit {
-
   templateText1: any;
   isMain = true;
   smsTemplateData2: any;
@@ -71,35 +61,7 @@ export class SmstemplateComponent implements OnInit {
       'smstemplateconfig_id': [null]
     });
   }
-
   editTemplate(text) {
-
-
-
-  heroine: TemplateData = {
-    id: 1,
-    header: 'Tom',
-    message: 'On behalf of Up coming Good Friday, We truly wishes a Happy n Joyful day',
-    footer: '***From Flujo***'
-  };
-
-  templatestring: string;
-  public templateText: string;
-  header: string;
-  message: string;
-  footer: string;
-
-  constructor() {
-    this.header = 'Good Friday';
-    this.message = 'On behalf of Up coming Good Friday, We truly wishes a Happy n Joyful day';
-    this.footer = '***From Flujo***';
-    this.templateText = '';
-    this.templatestring = this.header + this.message + this.footer;
-  }
-
-  editTemplate( text ) {
-    console.log((this.templatestring));
-
     this.templateText = text;
   }
 
