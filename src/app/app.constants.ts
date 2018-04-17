2f004aa93a66af2ff981828e5275c159a941de82
Yogesh <yogesh@flujo.in>
2018-04-17 14:32:08 +0530

    user login new response

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,5 +1,9 @@
 export class AppConstants {
-    public static get API_URL(): string { return 'http://flujo.in/dashboard/flujo.in_api_client/'; }
+    /** Main url */
+    // public static get API_URL(): string { return 'http://flujo.in/dashboard/flujo.in_api_client/'; }
+    /** Staging url */
+    public static get API_URL(): string { return 'http://flujo.in/dashboard/flujo_staging/'; }
+
     public static get CLIENT_ID(): string { return '1232'; }
     public static get THEME_ID(): string { return '32'; }
     public static get ACCESS_TOKEN(): string { return 'keerthan_token'; }
