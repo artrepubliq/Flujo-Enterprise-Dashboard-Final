1ff4b9daa4fe625eebb726561def1211b180ec2a
flujo <flujo@flujos-MacBook-Air-3.local>
2018-05-11 13:53:07 +0530

    added remaining elements in facebok tab

--- src/app/facebook/facebook.component.ts
+++ src/app/facebook/facebook.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FacebookComponent implements OnInit {
   public config: any;
+  public doComment: false;
   constructor() { }
 
   ngOnInit() {
