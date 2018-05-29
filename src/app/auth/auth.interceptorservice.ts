9312ece2e35f3fb24016b2c74027e5525e97f997
keerthan <flujo@flujos-MacBook-Air-3.local>
2018-05-29 15:16:37 +0530

    headers param set

--- src/app/auth/auth.interceptorservice.ts
+++ src/app/auth/auth.interceptorservice.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import { tokenNotExpired, JwtHelper } from 'angular2-jwt';
 import { AppConstants } from '../app.constants';
-import * as crypto from 'crypto';
+// import * as crypto from 'crypto';
 @Injectable()
 export class AuthInterceptorService {
   public getToken(): string {
     return localStorage.getItem('token');
+  }
+  public getDomain(): string {
+    return localStorage.getItem('domain_name');
   }
   public isAuthenticated(): boolean {
     // get the token
