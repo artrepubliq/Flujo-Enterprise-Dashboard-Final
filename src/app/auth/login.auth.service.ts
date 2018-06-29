440405959fd6c875882f199a86c2f41f6e1c1d48
Art Republiq <accounts@artrepubliq.com>
2018-06-29 19:03:17 +0530

    twiiter condition localstorage

--- src/app/auth/login.auth.service.ts
+++ src/app/auth/login.auth.service.ts
@@ -57,8 +57,10 @@ export class LoginAuthService implements OnInit {
     localStorage.setItem('user_id', authResult.user_id);
     localStorage.setItem('name', authResult.name);
     localStorage.setItem('expires_at', String(expTime));
-    localStorage.setItem('twitter_access_token', authResult.feature[0].feature_access_token);
+    if ((authResult.feature && authResult.feature[0].feature_access_token) {
+        localStorage.setItem('twitter_access_token', authResult.feature[0].feature_access_token);
     localStorage.setItem('token_expiry_date', authResult.feature[0].expiry_date);
+        }
     this.router.navigate(['/admin']);
     this.setLoggedInCustom(true);
   }
