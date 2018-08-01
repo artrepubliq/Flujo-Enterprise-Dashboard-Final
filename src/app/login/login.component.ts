9bb04ce50c2bfbf6776f54a91adf1228d5c963c3
Eshwar <mahesh@flujo.in>
2018-08-01 14:13:54 +0530

    url changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -56,7 +56,7 @@ export class LoginComponent implements OnInit {
     this.spinnerService.show();
     localStorage.clear();
     // this.loginForm.controls['origin_url'].setValue(window.location.href);
-    this.loginForm.controls['origin_url'].setValue('https://inktalks.flujo.in/#/login');
+    this.loginForm.controls['origin_url'].setValue('https://dashboard.vinaybhaskar.in');
     const formModel = this.loginForm.value;
     this.httpClient.post<ICommonInterface>(AppConstants.API_URL + 'flujo_client_login', formModel)
       .subscribe(
