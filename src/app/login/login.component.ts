0ef2295e00a463c44489b53fbdee305603c35130
Art Republiq <accounts@artrepubliq.com>
2018-08-01 14:15:27 +0530

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -55,8 +55,8 @@ export class LoginComponent implements OnInit {
   onSubmit = (body) => {
     this.spinnerService.show();
     localStorage.clear();
-    // this.loginForm.controls['origin_url'].setValue(window.location.href);
-    this.loginForm.controls['origin_url'].setValue('https://dashboard.vinaybhaskar.in');
+    this.loginForm.controls['origin_url'].setValue(window.location.href);
+//     this.loginForm.controls['origin_url'].setValue('https://dashboard.vinaybhaskar.in');
     const formModel = this.loginForm.value;
     this.httpClient.post<ICommonInterface>(AppConstants.API_URL + 'flujo_client_login', formModel)
       .subscribe(
